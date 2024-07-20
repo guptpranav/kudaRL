43cc0d158a8cce63a7d2806b32f873aada9b1465
guptpranav <pranavgupta.53535@gmail.com>
2024-07-20 17:07:41 +0530

    added Double Deep Q Learning, updated gitignore

--- Q-Learning/DeepQ/DQRL.py
+++ Q-Learning/DeepQ/DQRL.py
@@ -87,7 +87,7 @@ class FCQNetwork(nn.Module):
 
 class NFQ():
     """
-    initialises Neural Fitted Q-value RL agent
+    initialises Neural Fitted Q-value RL agent: action selection and model evaluation done using the same FCQNN
     :param learningRate: ALPHA
     :type learningRate: float
     :param discountRate: GAMMA
@@ -165,7 +165,10 @@ class NFQ():
             return self.FCQNN.predict(state).argmax().item()
     
     def update(self) -> None:
-        """trains and updates model over a batch of transitions"""
+        """
+        trains and updates model over a batch of transitions
+        target Q values bootstrapped to 1-step TD(0) update
+        """
         # update epsilon with linear decay
         self.epsilon = max(self.epsilon - (self.EPS_MAX-self.EPS_MIN)/(self.EPS_DEC), self.EPS_MIN)
 
@@ -176,10 +179,11 @@ class NFQ():
         # sample batch
         states, actions, rewards, states_, terminateds = self.expMemory.sampleBatch(batchSize=self.batchSize)
         
-        # calculate predicted and target Q-values according to off-policy 1-step TD(0) target values
-        # policyQ => [Q(s,a;θ)] with attachment to gradient chain maintained to perform gradient ascent
+        # policyQ => [Q(s,a;θ)]
+        # results are kept attached to gradient chain to perform gradient ascent
         policyQ = self.FCQNN.predict(states).gather(1, actions)
-        # targetQ => [r + γ*max_{a'}Q(s',a';θ)] detached from the gradient chain, made a pseudo-constant estimate of true Q-value
+        # targetQ => [r + γ*max_{a'}Q(s',a';θ)]
+        # results are detached from the gradient chain and treated like a pseudo-constant estimate of true Q-value
         targetQ = rewards + self.GAMMA*self.FCQNN.predict(states_).detach().max(1)[0].unsqueeze(1)*(1-terminateds)
         
         # calculate batch loss and back-propagate
@@ -190,7 +194,8 @@ class NFQ():
 
 class DQN():
     """
-    initialises Neural Fitted Q-value RL agent
+    initialises Deep Q Network RL agent: action selection and model evaluation performed with seperate FCQNNs, synced periodically\n
+    learning stabilised by temporary freezing of target Q network
     :param learningRate: ALPHA
     :type learningRate: float
     :param discountRate: GAMMA
@@ -281,7 +286,10 @@ class DQN():
             return self.policyFCQN.predict(state).argmax().item()
     
     def update(self) -> None:
-        """trains and updates model over a batch of transitions"""
+        """
+        trains and updates model over a batch of transitions
+        target Q values bootstrapped to 1-step TD(0) update
+        """
         # update epsilon with linear decay
         self.epsilon = max(self.epsilon - (self.EPS_MAX-self.EPS_MIN)/(self.EPS_DEC), self.EPS_MIN)
 
@@ -292,10 +300,9 @@ class DQN():
         # sample batch
         states, actions, rewards, states_, terminateds = self.expMemory.sampleBatch(batchSize=self.batchSize)
         
-        # calculate predicted and target Q-values according to off-policy 1-step TD(0) target values
-        # policyQ => [Q(s,a;θ)] with attachment to gradient chain maintained to perform gradient ascent
+        # policyQ => [Q(s,a;θ)]
         policyQ = self.policyFCQN.predict(states).gather(1, actions)
-        # targetQ => [r + γ*max_{a'}Q(s',a';θ)] detached from the gradient chain, made a pseudo-constant estimate of true Q-value
+        # targetQ => [r + γ*max_{a'}Q(s',a';θ')]
         targetQ = rewards + self.GAMMA*self.targetFCQN.predict(states_).detach().max(1)[0].unsqueeze(1)*(1-terminateds)
         
         # calculate batch loss and back-propagate
@@ -305,7 +312,125 @@ class DQN():
         self.optimiser.step()
 
 class DoubleDQN():
-    pass
+    """
+    initialises Deep Q Network RL agent: action selection and model evaluation performed with seperate FCQNNs, synced periodically\n
+    stabler learning: temporary freezing of target Q network\n
+    lower maximisation bias: while boostrapping, select best next actions using policy network and evaluate them with target network 
+    :param learningRate: ALPHA
+    :type learningRate: float
+    :param discountRate: GAMMA
+    :type discountRate: float
+    :param epsMax: initial epsilon
+    :type epsMax: float
+    :param epsMin: final epsilon
+    :type epsMin: float
+    :param epsDec: episodes to linearly decay epsilon
+    :type epsDec: int
+    :param numInputs: dimension of observation space
+    :type numInputs: int
+    :param numActions: cardinality of action space
+    :type numActions: int
+    :param nodes: sizes of hidden fully connected layers
+    :type nodes: tuple
+    :param sync: number of environment steps to sync policy and target networks
+    :type sync: int
+    :param capacity: maximum transitions to keep in replay memory
+    :type capacity: int
+    :param batchSize: size of batch of transitions to train FCQNN
+    :type batchSize: int
+    """
+    def __init__(self,
+                 learningRate:float, discountRate:float,
+                 epsMax:float, epsMin:float, epsDec:int,
+                 numInputs:int, numActions:int, nodes:tuple, sync:int,
+                 capacity:int, batchSize:int) -> None:
+        # hyperparameters
+        self.ALPHA      = learningRate
+        self.GAMMA      = discountRate
+        self.EPS_MAX    = epsMax
+        self.EPS_MIN    = epsMin
+        self.EPS_DEC    = epsDec
+        self.epsilon    = epsMax
+        self.numInputs  = numInputs
+        self.numActions = numActions
+        self.nodes      = nodes
+        self.sync       = sync
+        self.steps2sync = 0
+
+        # instantiating policy FCQNN with L2 loss and RMSProp optimiser
+        self.policyFCQN = FCQNetwork(numInputs=numInputs, numActions=numActions, nodes=nodes)
+        self.loss_fn    = nn.MSELoss()
+        self.optimiser  = optim.RMSprop(self.policyFCQN.parameters(), lr=self.ALPHA)
+
+        # instantiating target FCQNN as a copy of the policy FCQNN
+        self.targetFCQN = FCQNetwork(numInputs=numInputs, numActions=numActions, nodes=nodes)
+        self.targetFCQN.load_state_dict(self.policyFCQN.state_dict())
+
+        # instantiating replay memory
+        self.expMemory  = ReplayMemory(capacity=capacity)
+        self.batchSize  = batchSize
+    
+    def step(self, state, action, reward, state_, terminated) -> None:
+        """
+        pushes transition to the top of replay memory and handles syncing of policy and target networks 
+        :param s:  start state
+        :param a:  action taken
+        :param r:  reward value
+        :param s_: next state
+        :param t:  terminal?
+        """
+        self.expMemory.store(s=state, a=action, r=reward, s_=state_, t=terminated)
+        self.steps2sync += 1
+        if self.steps2sync == self.sync:
+            self.steps2sync = 0
+            self.targetFCQN.load_state_dict(self.policyFCQN.state_dict())
+    
+    def selectEpsilonGreedyAction(self, state) -> int:
+        """
+        selects and returns an epsilon-greedy action
+        :param state: state
+        """
+        if random.random() < self.epsilon:
+            action = random.randrange(start=0, stop=self.numActions)
+        else:
+            with torch.no_grad():
+                action = self.policyFCQN.predict(state).argmax().item()
+        return action
+    
+    def selectGreedyAction(self, state) -> int:
+        """
+        selects and returns greediest action
+        :param state: state
+        """
+        with torch.no_grad():
+            return self.policyFCQN.predict(state).argmax().item()
+    
+    def update(self) -> None:
+        """
+        trains and updates model over a batch of transitions
+        target Q values bootstrapped to 1-step TD(0) update
+        """
+        # update epsilon with linear decay
+        self.epsilon = max(self.epsilon - (self.EPS_MAX-self.EPS_MIN)/(self.EPS_DEC), self.EPS_MIN)
+
+        # skip training if minimal unique batch cannot be sampled
+        if len(self.expMemory) < self.batchSize:
+            return
+        
+        # sample batch
+        states, actions, rewards, states_, terminateds = self.expMemory.sampleBatch(batchSize=self.batchSize)
+        
+        # policyQ => [Q(s,a;θ)]
+        policyQ = self.policyFCQN.predict(states).gather(1, actions)
+        # targetQ => [r + γ*Q(s',argmax_{a'}Q(s',a';θ);θ')]
+        actions_ = self.policyFCQN.predict(states_).argmax(1).unsqueeze(1) # argmax_{a'}Q(s',a';θ)
+        targetQ = rewards + self.GAMMA*self.targetFCQN.predict(states_).detach().gather(1, actions_)*(1-terminateds)
+        
+        # calculate batch loss and back-propagate
+        loss = self.loss_fn(policyQ, targetQ)
+        self.optimiser.zero_grad()
+        loss.backward()
+        self.optimiser.step()
 
 class DuellingDoubleDQN():
     pass
